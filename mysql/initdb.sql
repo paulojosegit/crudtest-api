2ad47bd048409441f0406ecbb7099069266a80f2
paulojosegit <paulojosegm@gmail.com>
2025-12-04 23:04:46 -0300

    Ajustes no arquivo initdb.sql

--- mysql/initdb.sql
+++ mysql/initdb.sql
@@ -27,8 +27,8 @@ CREATE TABLE IF NOT EXIST orders (
 );
 
 INSERT INTO customer (name,lastname,birthday,email,sex,telephone)
-VALUES ('Paulo','Campos','1984/12/14','paulo@gmail.com','M', '11988770011'),
-       ('Julia','Assis','1992/22/04','ju@hotmail.com','F', '11988752342'),
+VALUES ('Paulo','Cardoso','1954/01/04','paulo@gmail.com','M', '11988770011'),
+       ('Juliana','Sousa','1992/23/06','ju@hotmail.com','F', '11988752342'),
        ('Laura','Gold','1990/01/01','eliana@gmail.com','F', '11988661221'),
-       ('Carlos','Campos','1952/02/13','carlos@hotmail.com','M', '11966451314'),
-       ('Roberta','ALves','1995/05/08','ro.alves@outlook.com','F', '11975443140')
+       ('Carlos','Cardoso','1952/02/13','carlos@hotmail.com','M', '11966451314'),
+       ('Roberta','Alves','1995/05/08','ro.alves@outlook.com','F', '11975443140')
