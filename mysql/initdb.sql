37ff00ce285b6908c408840d71608c3f97fbc95c
paulojosegit <paulojosegm@gmail.com>
2025-12-06 23:00:39 -0300

    Adicionado tabelas de order_items, melhorias nas relações entre tabelas.

--- mysql/initdb.sql
+++ mysql/initdb.sql
@@ -8,27 +8,55 @@ CREATE TABLE IF NOT EXISTS customer (
     telephone VARCHAR(11) NOT NULL
 );
 
-CREATE TABLE IF NOT EXIST Products (
+CREATE TABLE IF NOT EXISTS products (
     id_product INT AUTO_INCREMENT PRIMARY KEY,
-    name_product VARCHAR(50) NOT NOT NULL,
+    name_product VARCHAR(50) NOT NULL,
     description VARCHAR(100),
     sku VARCHAR(12),
     base_price DECIMAL(10,2)
 
-)
+);
 
-CREATE TABLE IF NOT EXIST orders (
-    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS orders (
+    id_order INT AUTO_INCREMENT PRIMARY KEY,
     id_customer INT NOT NULL,
     data_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     data_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    status_order VARCHAR(10) NOT NULL
-    FOREIGN KEY (id_customer) REFERENCES (id)
+    status_order VARCHAR(10) NOT NULL,
+    FOREIGN KEY (id_customer) REFERENCES customer(id)
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+  order_items_id INT AUTO_INCREMENT PRIMARY KEY,
+  id_order INT NOT NULL,
+  id_product INT NOT NULL,
+  amount INT NOT NULL,
+  base_price DECIMAL(10,2),
+
+  CONSTRAINT fk_items_order_order
+    FOREIGN KEY (id_order) REFERENCES orders(id_order)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+  CONSTRAINT fk_items_order_product
+    FOREIGN KEY (id_product) REFERENCES products(id_product)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+
 );
 
 INSERT INTO customer (name,lastname,birthday,email,sex,telephone)
-VALUES ('Paulo','Cardoso','1954/01/04','paulo@gmail.com','M', '11988770011'),
-       ('Juliana','Sousa','1992/23/06','ju@hotmail.com','F', '11988752342'),
-       ('Laura','Gold','1990/01/01','eliana@gmail.com','F', '11988661221'),
-       ('Carlos','Cardoso','1952/02/13','carlos@hotmail.com','M', '11966451314'),
-       ('Roberta','Alves','1995/05/08','ro.alves@outlook.com','F', '11975443140')
+VALUES ('Paulo','Cardoso','1954-01-04','paulo@gmail.com','M', '11988770011'),
+       ('Juliana','Sousa','1992-06-23','ju@hotmail.com','F', '11988752342'),
+       ('Laura','Gold','1990-01-01','eliana@gmail.com','F', '11988661221'),
+       ('Carlos','Cardoso','1952-02-13','carlos@hotmail.com','M', '11966451314'),
+       ('Roberta','Alves','1995-05-08','ro.alves@outlook.com','F', '11975443140');
+
+
+INSERT INTO products (name_product, description, sku, base_price)
+VALUES ('Galaxy A26 5G (256GB) Verde', 'Smartphone da Samsung Top de linha.', 'DV-010101010', 5000.00),
+       ('Vision AI TV 65 polegadas Neo QLED 4K QN1EF 2025', 'TV TOP de Linha', 'DV-060502025', 2500.00),
+       ('Console Nintendo Switch 2', 'A próxima evolução do console Nintendo Switch.', 'CS-030303030', 4550.00),
+       ('Caixa De Som Go Essential 2', 'Com potência de saída de 3.1 W.', 'GM-040404040', 390.90),
+       ('Smartwatch Positivo Watch Essential', 'Tela Ips 1.83 Monitoramento De Exercícios.', 'DV-050505050', 159.90),
+       ('Fone de Ouvido Sem Fio HUAWEI FreeBuds SE 2', 'Bluetooth - Preto', 'DV-020202020', 99.90)
