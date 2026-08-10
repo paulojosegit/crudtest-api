1196fe40e3945befa35d9158a0c90622691d486e
Paulo Campos <paulojosegm@gmail.com>
2026-08-10 15:50:22 -0300

    Teste de primeiro lgo com OTEL

--- src/main/java/com/crudcompany/crudtestapi/controller/CustomerController.java
+++ src/main/java/com/crudcompany/crudtestapi/controller/CustomerController.java
@@ -1,24 +1,34 @@
 package com.crudcompany.crudtestapi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.crudcompany.crudtestapi.domain.Customer;
+import com.crudcompany.crudtestapi.repository.CustomerRepository;
 import com.crudcompany.crudtestapi.service.CustomerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
+    private static final Logger log =
+            LoggerFactory.getLogger(CustomerService.class);
+
     public CustomerService service;
 
     public CustomerController(CustomerService service) {
+        log.info("LOG DE TESTEEEEEE");
         this.service = service;
     }
 
     @GetMapping
     public Flux<Customer> getAll() {
+        
         return service.findAll();
     }
 
--- src/main/java/com/crudcompany/crudtestapi/service/CustomerService.java
+++ src/main/java/com/crudcompany/crudtestapi/service/CustomerService.java
@@ -2,12 +2,18 @@ package com.crudcompany.crudtestapi.service;
 
 import com.crudcompany.crudtestapi.domain.Customer;
 import com.crudcompany.crudtestapi.repository.CustomerRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
 
 @Service
 public class CustomerService {
     private final CustomerRepository repository;
+
+    private Logger logger = LoggerFactory.getLogger(CustomerRepository.class.getName());
 
     public CustomerService(CustomerRepository repository)
     {
@@ -15,6 +21,8 @@ public class CustomerService {
     }
 
     public Flux<Customer> findAll() {
+        logger.info("LOG DE TESTEEEEE");
         return repository.findAll();
+
     }
 }
